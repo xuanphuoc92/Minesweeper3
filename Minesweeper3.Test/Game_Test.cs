3724f55c6ffa9a6fcc943a3528fcb3a879c7a8e5
xuanphuoc92 <xuanphuoc92@gmail.com>
2023-10-24 21:16:11 +0800

    Rename the default class files. Update the project reference.

--- Minesweeper3.Test/Game_Test.cs
+++ Minesweeper3.Test/Game_Test.cs
@@ -1,7 +1,7 @@
 namespace Minesweeper3.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class Game_Test
     {
         [TestMethod]
         public void TestMethod1()
--- Minesweeper3/Game.cs
+++ Minesweeper3/Game.cs
@@ -2,7 +2,7 @@
 
 namespace Minesweeper3
 {
-    public class Class1
+    public class Game
     {
 
     }
