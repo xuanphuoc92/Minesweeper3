ddf325536491cf9be41474dbd84c48c793b63233
xuanphuoc92 <xuanphuoc92@gmail.com>
2023-10-25 01:59:03 +0800

    Add the Game as the scoped service

--- Minesweeper3.Mud/Program.cs
+++ Minesweeper3.Mud/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Minesweeper3;
 using Minesweeper3.Mud;
 using MudBlazor.Services;
 
@@ -8,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => Game.New(10, 10).RandomizeMines(10).Start());
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
