ad617540fdab8feac224c743e441066c2f8e1bf2
xuanphuoc92 <xuanphuoc92@gmail.com>
2023-10-24 21:30:46 +0800

    Implement and test New()

--- Minesweeper3.Test/Game_Test.cs
+++ Minesweeper3.Test/Game_Test.cs
@@ -1,11 +1,20 @@
+using FluentAssertions;
+
 namespace Minesweeper3.Test
 {
     [TestClass]
     public class Game_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void _01_NewGame()
         {
+            Game game = Game.New(2, 1);
+
+            game.Width.Should().Be(2);
+            game.Height.Should().Be(1);
+            game.Start.Should().BeNull();
+            game.End.Should().BeNull();
+            game.State.Should().Be(GameState.Playing);
         }
     }
 }
--- Minesweeper3/Game.cs
+++ Minesweeper3/Game.cs
@@ -4,6 +4,24 @@ namespace Minesweeper3
 {
     public class Game
     {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public DateTime? Start { get; private set; }
+        public DateTime? End { get; private set; }
+        public GameState State { get; private set; }
 
+        public static Game New(int width, int heigth)
+        {
+            return new Game()
+            {
+                Width = width,
+                Height = heigth,
+            };
+        }
+    }
+
+    public enum GameState
+    {
+        Playing, Win, Lose
     }
 }
